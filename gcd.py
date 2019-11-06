dded4c819a905484dbde9bb6dbb85bfdf52abad8
ACEinfinity7 <acelko7@gmail.com>
2019-11-05 20:01:55 -0500

    did prework for write up and wrote gcd code, both iterative and recursive.

--- gcd.py
+++ gcd.py
@@ -1,1 +1,23 @@
+num_1 = 15
+num_2 = 24
 
+def find_gcd(a,b):
+    while a != b:
+        if b > a:
+            b -= a
+        else:
+            c = b
+            b = a
+            a = c
+            b -=a
+    return a
+
+
+
+def recur_gcd(a,b):
+    if a == b:
+        return a
+    if b > a:
+        return recur_gcd(a, b-a)
+    else:
+        return recur_gcd(b, a-b)
